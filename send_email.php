5b832499da2972c3a4c1a23961bfe0362948dfa8
Makhdoom Muhammad Khizar Hayat <muhammad.khizar@ropstam,com>
2024-02-28 23:39:26 +0500

    email submission

--- send_email.php
+++ send_email.php
@@ -8,20 +8,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST['message'];
 
     // Compose the email message
-    $to = "makhdoomkhizar1@gmail.com"; // Change this to your email address
+    $to = "your_email@example.com"; // Change this to your email address
     $subject = "New message from your website: $subject";
     $body = "First Name: $fname\nLast Name: $lname\nEmail: $email\n\n$message";
 
     // Send the email
     if (mail($to, $subject, $body)) {
-        http_response_code(200);
         echo "Message sent successfully!";
     } else {
-        http_response_code(500);
         echo "Oops! There was a problem sending your message.";
     }
 } else {
-    http_response_code(405); // Method Not Allowed
     echo "This method is not allowed.";
 }
 ?>
